9e63ccacf617ca896380928304fc6e782ac8e5b4
Rex <rex.xie@qq.com>
2017-12-18 08:00:00 +0800

    wholepanelHistoryInfoView

---
+++ BrightMaster/forms/WholePanelHistoryInfoView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BrightMaster.forms
+{
+    /// <summary>
+    /// Interaction logic for WholePanelHistoryInfoView.xaml
+    /// </summary>
+    public partial class WholePanelHistoryInfoView : Window
+    {
+        public WholePanelHistoryInfoView()
+        {
+            InitializeComponent();
+            this.Loaded += WholePanelHistoryInfoView_Loaded;
+        }
+
+        void WholePanelHistoryInfoView_Loaded(object sender, RoutedEventArgs e)
+        {
+            lvHistory.ItemsSource = GlobalVars.Instance.WholePanelHistoryInfoCollection.AllInfos;
+        }
+    }
+}
